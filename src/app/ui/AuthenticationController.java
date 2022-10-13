f4f751522823b73b0bcfcd95246ef2710e4c9177
Dien Chau <dienkchau@gmail.com>
2022-10-12 22:43:25 -0500

    Changed starting_page.fxml to better reflect StartController variable changes, and added integration between authentication's fxml and controller

---
+++ src/app/ui/AuthenticationController.java
@@ -0,0 +1,31 @@
+package app.ui;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
+public class AuthenticationController {
+    @FXML
+	private Button backBtn;
+	@FXML
+	private TextField userEntry;
+	@FXML
+	private Button submitBtn;
+
+
+	public void initialize() {
+	}
+
+    public void backClick() throws IOException {
+        System.out.println("Authentication ---> Start Page");
+        // managerBtn.getScene().setRoot(FXMLLoader.load(getClass().getResource("authentication.fxml")));
+    }
+
+    public void submitClick() throws IOException {
+        System.out.println("Authenticating...");
+        // managerBtn.getScene().setRoot(FXMLLoader.load(getClass().getResource("authentication.fxml")));
+    }
+}
