4eccebafcb0c2cf3cc63b6fd4adcc9980e3e2c4c
Ethan Cherian <ethan.cherian@gmail.com>
2022-10-12 19:10:39 -0500

    Got FXML changes to happen smoothly

--- src/app/ui/Controller.java
+++ src/app/ui/Controller.java
@@ -1,16 +1,28 @@
 package app.ui;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class Controller {
 
 	@FXML
 	private Label label;
+	@FXML
+	private Button btn;
 
 	public void initialize() {
-		String javaVersion = System.getProperty("java.version");
-		String javafxVersion = System.getProperty("javafx.version");
-		label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+
+	}
+
+	public void handleEvent() throws IOException {
+		System.out.println("Handling event");
+		
+		btn.setText("yo");
+		label.setText("do it again!");
+		btn.getScene().setRoot(FXMLLoader.load(getClass().getResource("temp.fxml")));
 	}
 }
