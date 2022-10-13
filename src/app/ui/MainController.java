5976d33a332d7e2c10735136927c15fa69da9aa7
Ethan Cherian <ethan.cherian@gmail.com>
2022-10-12 21:42:51 -0500

    Created manager ui and controller

--- src/app/ui/MainController.java
+++ src/app/ui/MainController.java
@@ -6,8 +6,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Controller {
+public class MainController {
 
 	@FXML
 	private Label label;
@@ -24,5 +27,12 @@ public class Controller {
 		btn.setText("yo");
 		label.setText("do it again!");
 		btn.getScene().setRoot(FXMLLoader.load(getClass().getResource("temp.fxml")));
+	}
+
+	public void handleEvent2() throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("server.fxml"));
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root));
+		stage.show();
 	}
 }
---
+++ src/app/ui/ManagerController.java
@@ -0,0 +1,36 @@
+package app.ui;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+public class ManagerController {
+    @FXML
+    private Button trendsBtn;
+    @FXML
+    private Button inventoryBtn;
+    @FXML
+    private Button backBtn;
+    @FXML
+    private Label manLabel;
+
+    public void initialize() {
+        // idk what to put here lol
+    }
+
+    public void trendsClick() throws IOException {
+        System.out.println("Manager --> Trends");
+        trendsBtn.getScene().setRoot(FXMLLoader.load(getClass().getResource("trends.fxml")));
+    }
+    public void inventoryClick() throws IOException {
+        System.out.println("Manager --> Inventory");
+        inventoryBtn.getScene().setRoot(FXMLLoader.load(getClass().getResource("inventory.fxml")));
+    }
+    public void backClick() throws IOException {
+        System.out.println("Manager --> Home");
+        backBtn.getScene().setRoot(FXMLLoader.load(getClass().getResource("starting_page.fxml")));
+    }
+}
