d5f446e3a4b63a8ee64bfb6f99b14f1b835b5dd4
EthanCherian <ethan.cherian@gmail.com>
2022-10-13 22:33:30 -0500

    Fixed UUID stuff

--- src/app/Main.java
+++ src/app/Main.java
@@ -20,8 +20,8 @@ import java.sql.DriverManager;
 public class Main extends Application {
 	public static boolean wow;
 	public static UUID defaultId=UUID.fromString("00000000-0000-0000-0000-000000000000");
-	public static String authen; // Store the result fxml file after authen
 	public static String username;
+	public static String authen; // Store the result fxml file after authen
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
