8c860e46da02cbcc82803a1ec2b0d98c0e90c89f
EthanCherian <ethan.cherian@gmail.com>
2022-11-05 11:48:32 -0500

    Added query to test order-menu connection

--- test_queries.sql
+++ test_queries.sql
@@ -4,3 +4,9 @@ LEFT JOIN menu_inventory ON menu.item_id = menu_inventory.item_id
 LEFT JOIN inventory ON menu_inventory.ingredient_id = inventory.ingredient_id
 ;
 
+-- Getting orders and their associated ingredients
+SELECT order.id, order.customer_name, menu.item_name FROM menu
+LEFT JOIN order_menu ON order_menu.order_id = order.id
+LEFT JOIN menu ON order_menu.item_id = menu.item_id
+;
+
