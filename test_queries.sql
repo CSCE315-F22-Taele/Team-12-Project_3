823d623847d8a663ca1b408567af1ca09abef1b0
root <root@DESKTOP-Q63U7OS.localdomain>
2022-11-05 13:11:54 -0500

    Made changes to test_queries

--- test_queries.sql
+++ test_queries.sql
@@ -8,5 +8,7 @@ LEFT JOIN inventory ON menu_inventory.ingredient_id = inventory.ingredient_id
 SELECT order.id, order.customer_name, menu.item_name FROM menu
 LEFT JOIN order_menu ON order_menu.order_id = order.id
 LEFT JOIN menu ON order_menu.item_id = menu.item_id
+-- LEFT JOIN menu_inventory ON menu_inventory.ingredient_id = menu.item_id
+-- LEFT JOIN inventory ON inventory.ingredient_id = menu_inventory.ingredient_id
 ;
 
