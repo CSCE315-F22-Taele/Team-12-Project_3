73a0a6bc588b2a86978bc02d056e3dbaddcfe16c
Dien Chau <dienkchau@gmail.com>
2022-11-05 21:45:18 -0500

    Changed the relational mappers for the orderdb and order_menudb by using association proxy, making the code a lot easier to read and understand

--- src/models/order_menudb.py
+++ src/models/order_menudb.py
@@ -1,4 +1,4 @@
-from . import db
+from . import db, Menu
 
 class OrderMenu(db.Model):
 
@@ -8,6 +8,9 @@ class OrderMenu(db.Model):
     item_id = db.Column(db.String(36), db.ForeignKey('menu.item_id'), primary_key=True)
     quantity = db.Column(db.Integer, nullable=False, server_default="0")
     total_price = db.Column(db.Integer, nullable=False, server_default="0")
+
+    # Reference to the "Menu" item object
+    menuItem = db.relationship("Menu")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
