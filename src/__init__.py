17c28b2c8a8747609db55754317c2237ac5b8347
root <root@DESKTOP-Q63U7OS.localdomain>
2022-11-08 21:17:24 -0600

    Added cors so that frontend can work with backend

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import os
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
 
     if test_config is None:
         app.config.from_mapping(
