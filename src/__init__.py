1508f5184e79aad5f2d9d6b8146eb6d5cbeef66e
Dien Chau <dienkchau@gmail.com>
2022-11-19 17:46:23 -0600

    Migrated old api to better follow rest conventions and added swagger-ui auto generated documentation. To support this had to deal with marshmallow for serialization and deserialization and webargs to make validate request args

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_apispec.extension import FlaskApiSpec
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
 import os
 
 def create_app(test_config=None):
@@ -29,15 +32,30 @@ def create_app(test_config=None):
         order
     )
 
-    baseBP = Blueprint('root', __name__, url_prefix='/api')
+    app.config.update({
+        'APISPEC_SPEC': APISpec(
+            title='315 POS',
+            version='v1',
+            plugins=[MarshmallowPlugin()],
+            openapi_version='2.0.0'
+        ),
+        'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON 
+        'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
+    })
 
-    
+
+    baseBP = Blueprint('root', __name__, url_prefix='/api')
     baseBP.register_blueprint(user.bp)
     baseBP.register_blueprint(inventory.bp)
     baseBP.register_blueprint(menu.bp)
     baseBP.register_blueprint(order.bp)
-
     app.register_blueprint(baseBP)
+
+    docs = FlaskApiSpec(app)
+    # docs.register(user.CreateUserResource, blueprint='user', endpoint='CreateUserResource')
+    # docs.register(user.create_user, blueprint='user')
+    docs.register(user.UserResource, blueprint='user')
+    docs.register(user.CreateUserResource, blueprint='user')
 
     db.app = app
     db.init_app(app)
