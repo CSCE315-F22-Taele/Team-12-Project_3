9bf7abd73b629ced031f4d414f8ceb4d15158721
EthanCherian <ethan.cherian@gmail.com>
2022-11-30 12:27:38 -0600

    Moved Rev image and made buttons lineup prettier

--- ui/pages/customer/index.tsx
+++ ui/pages/customer/index.tsx
@@ -1,28 +1,31 @@
-import { Button, FormControl } from "@mui/material";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-import { GetServerSidePropsContext } from "next";
+import { Typography, Button } from "@mui/material";
 import { useRouter } from "next/router";
-import { useState } from "react";
-import {
-	addOrderAPI,
-	flaskAPI,
-	getMenuAPI,
-	serverSideInstance,
-} from "../../components/utils";
+import Reveille from "../../public/ReveillePic.jpg";
 import { StyledDiv } from "../../styles/mystyles";
-import { Typography } from "@mui/material";
+import Image from "next/dist/client/image";
+
+const justMainPageStyleDiv = {
+	marginTop: "3.5%",
+};
+const justMainPageStyleButtons = {
+	size: "large",
+};
 
 export default function HomePage({}) {
     const router = useRouter();
     return (
         <>
             <Typography variant="h1">Welcome to Rev's American Grill!</Typography>
+            <StyledDiv sx={justMainPageStyleDiv}>
+                <Image
+                    style={{ width: "auto", height: "50vh", position: "relative", zIndex: 1}}
+                    src={Reveille}
+                    alt="Reveille"
+                />
+            </StyledDiv>
             <StyledDiv>
                 <Button
-                    sx={{
-                        width: 500,
-                        height: 500
-                    }}
+                    sx={justMainPageStyleButtons}
 					onClick={() => {
 						router.push("/customer/cart");
 					}}>
