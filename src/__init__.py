735e1315445a8e855e5d130cc49c2d350ddd629f
Ethan Cherian <90527850+EthanCherian@users.noreply.github.com>
2022-12-06 21:10:38 -0600

    render >:(

--- src/__init__.py
+++ src/__init__.py
@@ -21,8 +21,8 @@ def create_app(test_config=None):
 
     if test_config is None:
         app.config.from_mapping(
-            SECRET_KEY = os.environ.get("SECRET_KEY"),
-            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
+            SECRET_KEY = os.getenv("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.getenv("SQLALCHEMY_DB_URI"),
         )
     else:
         app.config.from_mapping(test_config)
@@ -85,7 +85,7 @@ def create_app(test_config=None):
 
     @app.get("/")
     def index():
-        return "Hello World"
+        return "Hello World " + os.getenv("SQLALCHEMY_DB_URI")
 
     return app
 
